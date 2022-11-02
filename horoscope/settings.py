d9fc3a4f27b7ddccb8574df0f1d3e871df38d624
Mikhail Olshevski <108739427+MOlshevski@users.noreply.github.com>
2022-11-02 19:18:49 +0300

    Update settings.py

--- horoscope/settings.py
+++ horoscope/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#3ox&t$arvlr9*)p00@urfio0(@9!h&+rb_cl968hwlb(ck0f*'
+SECRET_KEY = '***********************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
